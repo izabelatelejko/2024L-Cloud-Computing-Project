0f40270a5fd0391a97a9101f2f65377d1da058ee
telejko <iza.telejko@gmail.com>
2024-03-27 00:05:20 +0100

    add test pipeline

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash     
+PROJECT_ID="test-house"
+REGION="europe-west3"
+REPOSITORY="houseprice"
+IMAGE='training'
+IMAGE_TAG='training:latest'
+
+docker build -t $IMAGE .
+docker tag $IMAGE $REGION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY/$IMAGE_TAG
---
+++ push_image.sh
@@ -0,0 +1,16 @@
+#!/bin/bash     
+PROJECT_ID="test-house"
+REGION="europe-west3"
+REPOSITORY="houseprice"
+IMAGE_TAG='training:latest'
+
+# Create repository in the artifact registry
+gcloud beta artifacts repositories create $REPOSITORY \
+  --repository-format=docker \
+  --location=$REGION
+
+# Configure Docker
+gcloud auth configure-docker $REGION-docker.pkg.dev
+
+# Push
+docker push $REGION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY/$IMAGE_TAG
