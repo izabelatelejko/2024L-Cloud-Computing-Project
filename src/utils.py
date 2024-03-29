d3561643c5578d2266cf62b3fedc66abafac4f91
grzegorzZ1 <72541790+grzegorzZ1@users.noreply.github.com>
2024-03-30 00:39:08 +0100

    Added big query/storage connection to pipelines

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 """Pipeline utils."""
 
 import numpy as np
+from google.cloud import bigquery
+
 
 def remove_highly_correlated_features(df, threshold=0.9):
     corr_matrix = df.corr().abs()
@@ -8,17 +10,28 @@ def remove_highly_correlated_features(df, threshold=0.9):
     to_drop = [column for column in upper.columns if any(upper[column] > threshold)]
     return to_drop
 
+
 def remove_constant_features(df):
     return [column for column in df.columns if df[column].nunique() == 1]
 
+
 def standarise_float_columns(df):
     for column in df.columns:
-        if df[column].dtype == 'float64':
+        if df[column].dtype == "float64":
             df[column] = (df[column] - df[column].mean()) / df[column].std()
     return df
 
+
 def normalise_int_columns(df):
     for column in df.columns:
-        if df[column].dtype == 'int64':
-            df[column] = (df[column] - df[column].min()) / (df[column].max() - df[column].min())
+        if df[column].dtype == "int64":
+            df[column] = (df[column] - df[column].min()) / (
+                df[column].max() - df[column].min()
+            )
     return df
+
+
+def create_table_from_df(bq_client, df, table_id, write_disposition="WRITE_APPEND"):
+    job_config = bigquery.LoadJobConfig(write_disposition=write_disposition)
+    job = bq_client.load_table_from_dataframe(df, table_id, job_config=job_config)
+    job.result()
