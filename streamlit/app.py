3feda32ce13626fa2a0c9b1b2772b276926a2202
telejko <iza.telejko@gmail.com>
2024-05-26 06:00:08 +0200

    add pokemon image urls

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,4 +1,20 @@
 import streamlit as st
 
+st.set_page_config(layout="wide")
 st.title("Hello, Streamlit on App Engine!")
 st.write("This is a simple Streamlit app running on Google App Engine.")
+
+images = [
+    "https://archives.bulbagarden.net/media/upload/0/05/0005Charmeleon.png",
+    "https://archives.bulbagarden.net/media/upload/d/d2/0023Ekans.png",
+    "https://archives.bulbagarden.net/media/upload/1/14/0116Horsea.png",
+    "https://archives.bulbagarden.net/media/upload/5/54/0007Squirtle.png",
+    "https://archives.bulbagarden.net/media/upload/4/4a/0025Pikachu.png",
+]
+names = ["Charmeleon", "Ekans", "Horsea", "Squirtle", "Pikachu"]
+st.image(
+    images,
+    caption=names,
+    width=200,
+)
+st.caption("Images from Bulbapedia")
--- streamlit/run_streamlit.sh
+++ streamlit/run_streamlit.sh
@@ -1,10 +1,11 @@
 PROJECT_ID="cloud-computing-project-418718"
 REGION="europe-west3"
-REPOSITORY="streamlit"
+REPOSITORY="docker-img"
 IMAGE_TAG='streamlit_app:latest'
 
 # Build a local docker image
 docker build -t streamlit_image .
+docker tag streamlit_image $REGION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY/$IMAGE_TAG
 
 # Configure Docker
 gcloud auth configure-docker $REGION-docker.pkg.dev
