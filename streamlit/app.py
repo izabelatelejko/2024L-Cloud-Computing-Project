236e5239e53293dddb0a1692a33563a012b71892
kasprzakj <kasprzakkuba01@gmail.com>
2024-06-01 19:48:55 +0200

    Correct app imports

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,8 +1,4 @@
-from st_pages import Page, show_pages, add_page_title, hide_pages
-from streamlit_extras.switch_page_button import switch_page
-import yaml
-from yaml.loader import SafeLoader
-import streamlit_authenticator as stauth
+from st_pages import Page, show_pages
 import streamlit as st
 
 st.set_page_config(page_title='PokéFinder')
