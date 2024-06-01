7bf7313084e71fd986d9514445060d6eee9ff598
grzegorzZ1 <72541790+grzegorzZ1@users.noreply.github.com>
2024-06-01 22:08:55 +0200

    Changed data loading

--- load_mock_data.py
+++ load_mock_data.py
@@ -2,11 +2,19 @@ from google.cloud import bigquery
 import pandas as pd
 
 df = pd.read_csv("pokemon_data/pokemon.csv")
-df = df.loc[10000:60000, :]
+df = df.loc[:50000, :]
 bq_client = bigquery.Client(
     location="europe-west3", project="cloud-computing-project-418718"
 )
-job_config = bigquery.LoadJobConfig(write_disposition="WRITE_TRUNCATE")
+
+query_delete = f"""
+  delete from `cloud-computing-project-418718.common_ds.main_table` where 1=1
+"""
+
+job = bq_client.query(query_delete)
+job.result()
+
+job_config = bigquery.LoadJobConfig(write_disposition="WRITE_APPEND")
 job = bq_client.load_table_from_dataframe(
     df, "cloud-computing-project-418718.common_ds.main_table", job_config=job_config
 )
